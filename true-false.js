5d8ba7a34fed0c632b57a44850846b014b155785
tania-developer <69915059+tania-developer@users.noreply.github.com>
2021-02-03 06:45:56 +0600

    truthy-falsy

---
+++ true-false.js
@@ -0,0 +1,16 @@
+//false 
+//0
+//""
+//undefined
+//null
+//NaN
+//Truthy
+//'0',' ',[]
+const age = -4;
+
+if(age){
+    console.log('condition is true');
+}
+else{
+    console.log("condition is false")
+}
